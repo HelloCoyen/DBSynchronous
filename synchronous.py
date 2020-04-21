f0e9a752cc2c934284b3b10b6300b9f122b46921
Wo_O3 <wuhaoyu@timesgroup.cn>
2020-04-21 17:56:56 +0800

    更新

--- synchronous.py
+++ synchronous.py
@@ -3,9 +3,10 @@
     author：Wo_O3
     date: 2019-11-7 16:53:02
     vesion:
-        2019-11-15 15:05:21 v1.0.1 格式化化输出界面,更换数据库引擎,简化逻辑
-        2019-11-19 11:03:47 v1.0.2 修改get_table函数,增加筛选
-        2019-11-25 16:08:52 v1.0.3 修复bug,过滤空表,优化正则规则
+        2019-11-15 15:05:21 优化输出界面,更换数据库引擎,简化逻辑
+        2019-11-19 11:03:47 修改get_table函数,增加筛选
+        2019-11-25 16:08:52 修复bug,过滤空表,优化正则规则
+        2020-4-17 11:38:33 优化注释结构
     function: 早期同步脚本升级版,使用pandas作为同步工具库,增加配置文件,分离代码
         与作业,实现数据库A到数据B的同步程序
 """
@@ -19,7 +20,7 @@ import pandas as pd
 import pymysql
 from sqlalchemy import create_engine
 
-__version__ = '2.0.1'
+__version__ = '1.0.1'
 
 def user_interface(args):
     '''
@@ -59,13 +60,14 @@ def get_database_info(cursor, database):
     cursor.execute(database_size_sql)
     datasize = cursor.fetchall()[0]['database_size']
     cursor.execute(
-        f'select table_name from information_schema.tables where table_schema = "{database}"'
+        f'select TABLE_NAME from information_schema.tables where table_schema = "{database}"'
         )
     result = cursor.fetchall()
+    result
     database_info = {}
     database_info['name'] = database
     database_info['size'] = datasize
-    database_info['tables'] = [r['table_name'] for r in result]
+    database_info['tables'] = [list(r.values())[0] for r in result]     
     database_info['number'] = len(result)
     return database_info
 
@@ -73,16 +75,24 @@ def get_table_info(cursor, database, table):
     '''
     获取表格大小以及表格列属性
     '''
-    cursor.execute(f"SHOW COLUMNS FROM {database}.{table}")
+    column_sql = f"""
+        select COLUMN_NAME ,COLUMN_TYPE ,COLUMN_COMMENT 
+        from information_schema.`COLUMNS` 
+        where TABLE_NAME = '{table}' and table_schema = '{database}'
+        """
+    cursor.execute(column_sql)
     columns_info = cursor.fetchall()
     table_size_sql = f"""
-        select table_name, concat(truncate(data_length,2),' bytes') as table_size
+        select TABLE_NAME, concat(truncate(data_length,2),' bytes') as table_size, TABLE_COMMENT
         from information_schema.tables
-        where table_name = '{table}' and table_schema = "{database}"
+        where TABLE_NAME = '{table}' and table_schema = "{database}"
         """
     cursor.execute(table_size_sql)
-    table_size = cursor.fetchall()[0]['table_size']
-    return columns_info, table_size
+    # table_size = cursor.fetchall()[0]['table_size']
+    # TABLE_COMMENT =  cursor.fetchall()[0]['TABLE_COMMENT']
+    table_info = cursor.fetchall()
+    # return columns_info, table_size, TABLE_COMMENT
+    return columns_info, table_info
 
 def get_table(cursor, database, table, **columns):
     '''
@@ -121,13 +131,14 @@ def create_target_database(cursor, database):
     '''
     cursor.execute(f'CREATE DATABASE IF NOT EXISTS {database}')
 
-def create_target_table(cursor, database, table, columns_info):
+def create_target_table(cursor, database, table_info, columns_info):
     '''
     创建目标表
     '''
-    columns = [c['Field'] + ' ' + c['Type'] for c in columns_info]
+    columns = ['`'+c['COLUMN_NAME'] + '` ' + c['COLUMN_TYPE'] + ' comment "' + c['COLUMN_COMMENT'] + '"'  for c in columns_info]
     columns = ",".join(columns)
-    cursor.execute(f"CREATE TABLE IF NOT EXISTS {database}.{table} ({columns})")
+    print(f"CREATE TABLE IF NOT EXISTS {database}.{table_info[0]['TABLE_NAME']} ({columns})comment='{table_info[0]['TABLE_COMMENT']}'")
+    cursor.execute(f"CREATE TABLE IF NOT EXISTS {database}.{table_info[0]['TABLE_NAME']} ({columns})comment='{table_info[0]['TABLE_COMMENT']}'")
 
 def drop_table(cursor, database, table):
     '''
@@ -140,6 +151,24 @@ def drop_database(cursor, database):
     删除目标数据库
     '''
     cursor.execute(f'DROP DATABASE IF EXISTS {database}')
+
+def add_table_comment(cursor, database, table_info):
+    cursor.execute(f'alter table {database}.{table_info["TABLE_NAME"]} comment "{table_info["TABLE_COMMENT"]}"')
+
+def add_columns_comment(cursor, database, table, columns_info):
+    column_sql = f"""
+        select COLUMN_NAME ,COLUMN_TYPE ,COLUMN_COMMENT 
+        from information_schema.`COLUMNS` 
+        where TABLE_NAME = '{table}' and table_schema = '{database}'
+        """
+    cursor.execute(column_sql)
+    columns_info_target = cursor.fetchall()
+    df1 = pd.DataFrame(columns_info_target)
+    df2 = pd.DataFrame(columns_info)
+    df = pd.merge(df1, df2, on='COLUMN_NAME',how='left')
+    for i in range(len(df)):
+        print(f'alter table {database}.{table} modify column `{df["COLUMN_NAME"].loc[i]}` {df["COLUMN_TYPE_x"].loc[i]} comment "{df["COLUMN_COMMENT_y"].loc[i]}"')
+        cursor.execute(f'alter table {database}.{table} modify column `{df["COLUMN_NAME"].loc[i]}` {df["COLUMN_TYPE_x"].loc[i]} comment "{df["COLUMN_COMMENT_y"].loc[i]}"')
 
 class DatabaseConnect:
     '''
@@ -241,35 +270,35 @@ class Synchronous(DatabaseConnect):
         '''
         # 同步模式确认
         mode = self.config['common']['mode']
-       if mode == 'replace':
-           tag = input(f'{"WARNING":^8}：当前同步模式为【覆盖模式】,该操作将会清除'
-                       '原始已有的原始数据,不可恢复!'
-                       '\n          确认同步请输入【Y】,输入任意键退出:')
-           if tag != 'Y':
-               self.close()
-               return
-       elif mode == 'append':
-           tag = input(f'{"WARNING":^8}：当前同步模式为【插入模式】,请检查表结构!'
-                       '\n          确认同步请输入【Y】,输入任意键退出:')
-           if tag != 'Y':
-               self.close()
-               return
-       else:
-           self.close()
-           raise ValueError('mode参数只接受"replace"和"append",默认为"replace"')
+#        if mode == 'replace':
+#            tag = input(f'{"WARNING":^8}：当前同步模式为【覆盖模式】,该操作将会清除'
+#                        '原始已有的原始数据,不可恢复!'
+#                        '\n          确认同步请输入【Y】,输入任意键退出:')
+#            if tag != 'Y':
+#                self.close()
+#                return
+#        elif mode == 'append':
+#            tag = input(f'{"WARNING":^8}：当前同步模式为【插入模式】,请检查表结构!'
+#                        '\n          确认同步请输入【Y】,输入任意键退出:')
+#            if tag != 'Y':
+#                self.close()
+#                return
+#        else:
+#            self.close()
+#            raise ValueError('mode参数只接受"replace"和"append",默认为"replace"')
         # 基本属性
         source = self.source
         target = self.target
         source_cursor = self.source_cursor
+        target_cursor = self.target_cursor
         target_engine = create_engine(
             f"mysql+pymysql://{target['user']}:{target['password']}"
             f"@{target['host']}:3306/{target['database']}"
             )
         # 控制台输出同步信息
-        database_info = get_database_info(source_cursor, source['database'])
         work_size = 0
         for table in self.table_set:
-            table_size = get_table_info(source_cursor, source['database'], table)[1]
+            table_size = get_table_info(source_cursor, source['database'], table)[1][0]['table_size']
             work_size = work_size + float(table_size[:-6])
         database_info = get_database_info(source_cursor, source['database'])
         arg = self.config
@@ -285,14 +314,20 @@ class Synchronous(DatabaseConnect):
         start = time.time()
         for table in self.table_set:
             s = time.time()
-            table_size = get_table_info(source_cursor, source['database'], table)[1]
+            table_columns_info = get_table_info(source_cursor, source['database'], table)
+            table_size = table_columns_info[1][0]['table_size']
             table_spend = int(float(table_size[:-6])/1024/1024/80*60)
             print(f'--->当前同步表：{table:<20}\t数据表大小：{table_size}\n--->预计耗时：{table_spend:^10} second')
             data = get_table(source_cursor, source['database'], table, **columns)
-            if len(data):
-                data.to_sql(table, target_engine, if_exists=mode, chunksize=2000, index=0)
+            if len(data) and mode == 'replace':
+                drop_table(target_cursor, target['database'], table)
+                create_target_table(target_cursor, target['database'], table_columns_info[1], table_columns_info[0])
+                data.to_sql(table, target_engine, if_exists='append', chunksize=2000, index=0)
                 e = time.time()
                 print(f'--->目标表：{table:<20} 已完成同步\n--->实际耗时：{int(e-s):^10} second\n{"-"*86}>')
+            elif len(data) and mode == 'append':
+                data.to_sql(table, target_engine, if_exists='append', chunksize=2000, index=0)
+                e = time.time()
             else:
                 print(f'--->WARNING:目标表{table:<20}为空,已跳过同步\n{"-"*86}>')
             count += 1
@@ -328,15 +363,14 @@ if __name__ == '__main__':
         mission_config = {'source':source, 'target':target, 'common':common}
         if mission_config['common']['switch'] == 'on':
             print(f'{"NOTICE":^8}：当前启动执行任务【{mission}】')
-           # key = datetime.datetime.strftime(
-               # datetime.date(
-                   # datetime.datetime.now().year- (datetime.datetime.now().month==1),
-                   # datetime.datetime.now().month - 1 or 12, 1
-                   # ),
-               # '%Y%m01'
-               # )
-		   # value = "'" + f"{load_date}" + "'"
-           # Synchronous(mission_config).main(key = value)
+#            load_date = datetime.datetime.strftime(
+#                datetime.date(
+#                    datetime.datetime.now().year- (datetime.datetime.now().month==1),
+#                    datetime.datetime.now().month - 1 or 12, 1
+#                    ),
+#                '%Y%m01'
+#                )
+#            Synchronous(mission_config).main(load_date = f"'{load_date}'")
             Synchronous(mission_config).main()
         else:
             print(f'{"NOTICE":^8}：任务【{mission}】状态关闭,跳过当前任务')
